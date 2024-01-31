ef35a06e9081e0ce258333cd2a779a29338e59e8
tainaraoliver <tr30405@gmail.com>
2024-01-31 12:12:31 -0600

    Ativ 02.js

---
+++ Ativ 02.js
@@ -0,0 +1,13 @@
+let soma = 0
+
+while (true) {
+let numero =prompt("Insira um numero positivo ( insira um numro negativo)"{
+if (numero < 0){
+break;
+console.log("Programa encerrado,pois o usuario informe um valor negativo");
+}
+if(numero % 2=== 0){
+soma=soma+numero;
+}
+ console.log(" a soma dos numeros pares é:$(soma }");
+ console.log("a soma dos numeros numeros pares é:"+soma)
